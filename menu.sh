0f2f7abf0870692f4efd6e9cd4b85a3a2d56e15a
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-04 19:31:17 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,7 +2,7 @@
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo "Checking VPS"
 #########################
-IZIN=$(curl -sS https://raw.githubusercontent.com/Pakhaji30/supreme/aio/permission/ip | awk '{print $4}' | grep $MYIP)
+IZIN=$(curl -sS ipv4.icanhazip.com | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 else
