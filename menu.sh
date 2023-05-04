a3f7400dc2f077ee47ce1d36858b1c135f24fa64
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-04 22:12:38 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -85,7 +85,7 @@ clear
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "                 • KANG HORY VPN PREMIUM •                 "
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[33m OS            \e[0m:  `hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
+echo -e "\e[33m OS            \e[0m:  "`hostnamectl" | grep "Operating System" | cut -d ' ' -f5-`	
 echo -e "\e[33m IP            \e[0m:  $IPVPS"	
 echo -e "\e[33m ASN           \e[0m:  $ISP"
 
