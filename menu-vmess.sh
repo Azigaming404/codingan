50b5080052eb8efb6f203cfb28df67dae10318b6
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-06-14 18:20:20 +0700

    Update menu-vmess.sh

--- menu-vmess.sh
+++ menu-vmess.sh
@@ -253,7 +253,7 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 
 read -rp "   Input Username : " -e user
 read -rp " input password : " -e pass
-sed -i "cyber-$pass" /etc/pass
+aji = cyber-$pass
       
 if [ -z $user ]; then
 echo -e "$COLOR1│${NC} [Error] Username cannot be empty "
